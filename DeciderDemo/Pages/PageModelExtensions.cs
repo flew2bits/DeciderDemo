db61b1fcdc81d84822eff22b82ff13dbca0a5540
tander3 <titus.anderson@jefferson.kyschools.us>
2023-02-10 13:08:25 -0500

    yet another converter

--- DeciderDemo/Pages/PageModelExtensions.cs
+++ DeciderDemo/Pages/PageModelExtensions.cs
@@ -1,12 +1,13 @@
 using System.Text.Json;
 using System.Xml;
+using DateOnlyTimeOnly.AspNet.Converters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DeciderDemo.Pages;
 
 public static class PageModelExtensions
 {
-    private static readonly JsonSerializerOptions Options = new() { WriteIndented = true };
+    private static readonly JsonSerializerOptions Options = new() { WriteIndented = true, Converters = { new DateOnlyJsonConverter(), new TimeOnlyJsonConverter()} };
     public static IEnumerable<string> SerializeEvents(this PageModel model, IEnumerable<object> events)
         => events.Select(e => $"{e.GetType().Name}: {JsonSerializer.Serialize(e, Options)}").ToArray();
 }
